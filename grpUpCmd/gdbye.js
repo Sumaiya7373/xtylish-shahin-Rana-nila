318f11943209acf18db7dce82dbc0e128a18a742
Sumaiya7373 <ranashahin11102005@gmail.com>
2026-02-02 04:09:50 +0600

    Update gdbye.js

--- grpUpCmd/gdbye.js
+++ grpUpCmd/gdbye.js
@@ -2,9 +2,19 @@ module.exports = {
   event: 'remove',
   handle: async ({ api, event }) => {
     const removedMembers = event.participants;
+
     for (const member of removedMembers) {
+      const username = member.split('@')[0];
+
       await api.sendMessage(event.id, {
-        text: `Goodbye @${member.split('@')[0]}, we'll miss you!`,
+        text:
+`╔═══❖•ೋ°°ೋ•❖═══╗
+👋 *Goodbye @${username}*
+╚═══❖•ೋ°°ೋ•❖═══╝
+
+🤗 আমাদের ছোট্ট পরিবার তোমাকে মিস করবে!
+
+— ☕ * *✎ᝰ.⎯꯭̽𝐒𝐡𝐚𝐡𝐢𝐧 𝐑𝐚𝐧𝐚⎯꯭̽᪳❤️‍🩹🪽*`,
         mentions: [member]
       });
     }
