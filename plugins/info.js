c3147da0d5813a2b98697b8cac3235c01a04e7f4
Sumaiya7373 <ranashahin11102005@gmail.com>
2026-02-02 03:47:25 +0600

    Update developer credit and personal information

--- plugins/info.js
+++ plugins/info.js
@@ -7,7 +7,7 @@ module.exports = {
     permission: 0,
     prefix: 'both',
     categorie: 'Utilities',
-    credit: 'Developed by Mohammad Nayan',
+    credit: 'Developed by Shahin Rana',
     usages: [`${global.config.PREFIX}info - Show admin and server information.`],
   },
   start: async ({ event, api, message }) => {
@@ -24,19 +24,18 @@ module.exports = {
 
       const infoMessage = `
 --------------------------------------------
-𝐍𝐚𝐦𝐞           : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
+𝐍𝐚𝐦𝐞           : 𝐒𝐡𝐚𝐡𝐢𝐧 𝐑𝐚𝐧𝐚
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤       : 𝐒𝐢𝐥𝐞𝐧𝐭 𝐤𝐢𝐥𝐥𝐞𝐫 𝐑𝐚𝐧𝐚
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧       : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐒𝐲𝐥𝐡𝐞𝐭, 𝐃𝐡𝐚𝐤𝐚
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : 𝐒𝐲𝐥𝐡𝐞𝐭, 𝐉𝐨𝐤𝐢𝐠𝐨𝐧𝐣
 𝐆𝐞𝐧𝐝𝐞𝐫       : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 𝟏𝟖+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐌𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤         : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩   : wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦     : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤: https://www.facebook.com/www.xnxx.com169
+𝐆𝐦𝐚𝐢𝐥       : Shahinrana5070@gmail.com
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩   : wa.me/+8801793447339
+
 
 --------------------------------------------
 \`\`\`
